96b3117f0c5448a32440e270fc441450db8fa0de
adityaa-12 <adityaa.sde@gmail.com>
2025-05-07 01:04:36 +0530

    added api fetch file

--- src/service/weatherService.js
+++ src/service/weatherService.js
@@ -1,21 +1,26 @@
 const baseurl = import.meta.env.VITE_API_URL;
-
 const apiKey = import.meta.env.VITE_API_KEY;
 
-const getByName = async () => {
+export const getByName = async (query, setWeatherData) => {
   try {
-    let req = await fetch(`${baseurl}/current.json?key=${apiKey}&q=mumbai`);
+    let req = await fetch(`${baseurl}/current.json?key=${apiKey}&q=${query}`);
     let res = await req.json();
-    console.log(res);
+    const data = {
+      temp: res.current.temp_c,
+      temp_desc: res.current.condition.text,
+      icon_url: res.current.condition.icon,
+      dew_point: res.current.dewpoint_c,
+      humidity: res.current.humidity,
+      feelslike: res.current.feelslike_c,
+      pressure: res.current.pressure_mb,
+      uv: res.current.uv,
+      wind: res.current.wind_kph,
+      country: res.location.country,
+      city: res.location.name,
+      date: res.location.localtime,
+    };
+    setWeatherData(data);
   } catch (error) {
-    alert("Network Error :", error);
+    alert("Internal Server Error!");
   }
 };
-
-const getByPoints = async () => {
-    try {
-        let req = await fetch(`${baseurl}/current.json?key${apiKey}`)
-    } catch (error) {
-        
-    }
-}
