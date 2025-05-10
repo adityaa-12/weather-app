47cd014e8610936871a0ecb1f01bc613080ab156
adityaa-12 <adityaa.sde@gmail.com>
2025-05-10 21:09:58 +0530

    updated app with meta info

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Suggestion from "./components/Suggestion";
 import Favorite from "./components/Favorite";
 import Loading from "./components/Loading";
 import { WeatherProvider } from "./context/weatherContext";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import Meta from "./components/Meta";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   return (
     <WeatherProvider>
+      <Meta />
       <Analytics />
       <div>
         <Header set={setLoading} />
